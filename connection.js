d15522274f0ca5a819e3b655bfc7a7ac41361dc4
Alfarizzi27 <farizyoga04.gmail.com>
2024-03-03 20:21:55 +0700

    Deploy Vercell

--- connection.js
+++ connection.js
@@ -1,11 +1,15 @@
 const { Pool } = require('pg')
 
+// const pool = new Pool({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'nutechtes',
+//     password: 'postgres',
+//     port: 5432
+// })
+
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'nutechtes',
-    password: 'postgres',
-    port: 5432
+    connectionString: process.env.POSTGRES_URL,
 })
 
 module.exports = pool
